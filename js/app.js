200bedfb6e74b52073e029528e264da8fe488cc9
Jonathan <jonathanallen105@gmail.com>
2025-08-12 21:33:07 -0400

    completed steps 1 - 3

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const board = document.querySelector('.board')
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let turn = 'X'
+let winner = false
+let tie = false
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -12,9 +16,16 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+const init = () => {
+    console.log('it works')
+    board.forEach((square) => {
+        square = ''
+    })
 
+    //render()
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+init()
 
